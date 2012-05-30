b668f9e30b381e6438a337b5a652c4d60e956180
Harald Mosoarca <harald.mosoarca@hull.ac.uk>
2012-05-30 17:13:17 +0100

    Initial commit

---
+++ HelloWorld/GoodbyeWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoodbyeWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye cruel world");
+        }
+    }
+}
