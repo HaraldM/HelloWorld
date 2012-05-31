b17108087b1c56fa8684966c2269a0094a92a114
Harald Mosoarca <harald.mosoarca@hull.ac.uk>
2012-05-31 12:12:41 +0100

    Added files properly

---
+++ HelloWorld/GoodbyeWorld/MessageManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoodbyeWorld
+{
+    public class MessageManager
+    {
+        public string Message { get; set; }
+        
+        static void Main(string[] args)
+        {
+            var p = new MessageManager();
+            p.Message = "Hello cruel world.";
+        }
+
+        public void Say(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return;
+            Message = message;
+        }
+
+        public string Transform()
+        {
+            return Char.ToUpper(Message[0]).ToString() + Message.Substring(1).ToLower();
+        }
+    }
+}
---
+++ HelloWorld/GoodbyeWorldTests/MessageManagerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoodbyeWorld;
+
+namespace GoodbyeWorldTests
+{
+    [TestClass]
+    public class MessageManagerTests
+    {
+        public MessageManagerTests()
+        {
+        }
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private MessageManager man;
+
+        [TestInitialize()]
+        public void MessageManagerInitialiser()
+        {
+            man = new MessageManager();
+        }
+
+        [TestMethod]
+        public void TestSay_MessageGetsSet()
+        {
+            var parameter = "Goodbye";
+            man.Say(parameter);
+            Assert.AreEqual(parameter, man.Message);
+        }
+
+        [TestMethod]
+        public void TestSay_EmptyMessage_MessageNotSet()
+        {
+            string parameter = null;
+            man.Say(parameter);
+            Assert.AreEqual(parameter, man.Message);
+        }
+
+        [TestMethod]
+        public void TestSay_TransformsText()
+        {
+            var parameter = "gOOdbye";
+            man.Message = parameter;
+
+            var result = man.Transform();
+            Assert.AreEqual("Goodbye", result);
+        }
+
+        [TestMethod]
+        public void TestSay_TransformsText2()
+        {
+            var parameter = "gOOdbye";
+            man.Message = parameter;
+
+            var result = man.Transform();
+            Assert.AreEqual("goodbye", result);
+        }
+    }
+}
